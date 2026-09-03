331c317d3d33901dba7c1c026c18be49aa44427f
Arman <arman.va@humaindevsystems.com>
2026-09-03 12:11:18 +0400

    Add routing

--- public/index.php
+++ public/index.php
@@ -1,40 +1,43 @@
 <?php
-
 use DTOs\User\registeruserdto;
 use DTOs\UserDto;
+$request = $_SERVER['REQUEST_URI'];
+switch ($request) {
+    case '/view/register':
+    {
+        require '../src/DBConnection.php';
+        require "../src/Auth.php";
+        require "../src/DTOs/UserDto.php";
 
-require '../src/DBConnection.php';
-require "../src/Auth.php";
-require "../src/DTOs/UserDto.php";
+        $auth = new auth($pdo);
+        $message = "";
 
-$auth = new auth($pdo);
-$message = "";
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-
-    if(strlen($name) < 3 || strlen($name) > 32){
-        $message = "Wrong name length\n";
-    }
-    else if (!str_contains($email, "@") || $auth->check_email($email)) {
-        $message = "Wrong email format\n";
-    }
-    else if(strlen($password) < 3 || strlen($password) > 32){
-        $message = "Wrong password length\n";
-    }
-    else {
-        $new_user_dto = new registeruserdto($name, $email, $password);
-        try {
-            $auth->create_user($new_user_dto);
-            $message = "You are registered";
-        } catch (Exception $e) {
-            $message = "Account doesnt registered";
+            if (strlen($name) < 3 || strlen($name) > 32) {
+                $message = "Wrong name length\n";
+            } else if (!str_contains($email, "@") || $auth->check_email($email)) {
+                $message = "Wrong email format\n";
+            } else if (strlen($password) < 3 || strlen($password) > 32) {
+                $message = "Wrong password length\n";
+            } else {
+                $new_user_dto = new registeruserdto($name, $email, $password);
+                try {
+                    $auth->create_user($new_user_dto);
+                    $message = "You are registered";
+                } catch (Exception $e) {
+                    $message = "Account doesnt registered";
+                }
+            }
         }
+        require __DIR__ . '/view/register.php';
     }
+    case '/view/login':
+        require __DIR__ . '/view/login.php';
 }
-
 ?>
 
 <!DOCTYPE html>
